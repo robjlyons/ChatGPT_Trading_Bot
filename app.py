7e8140a7f536463a40dbc2de127ada7bc5d00e92
robjlyons <81177492+robjlyons@users.noreply.github.com>
2024-10-22 14:47:32 +0100

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, jsonify
 import gdown
 app = Flask(__name__)
 
-@app.route('/run-colab')
+@app.route('/')
 def run_colab():
     gdown.download('https://drive.google.com/file/d/1D2WfRcEjou6aJG6DxFAlo_gmVmzeH8vj', 'tradingBot.ipynb', quiet=False)
     return jsonify(message='colab notebook ran successfully')
